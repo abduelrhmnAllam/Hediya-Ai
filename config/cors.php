952dcc685912f678e9a6f0949cb8b4b76a7b3252
abduelrhmnAllam <83561371+abduelrhmnAllam@users.noreply.github.com>
2025-10-27 04:34:16 +0300

    Major update:ssa

---
+++ config/cors.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | هذا الملف يحدد إعدادات الـ CORS لتطبيقك.
+    | يمكنك السماح لأي دومين أو تقييدها بدومينات محددة.
+    |
+    */
+
+    'paths' => ['api/*', 'storage/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+
+    'allowed_methods' => ['*'],
+
+
+    'allowed_origins' => ['*'],
+
+
+    'allowed_origins_patterns' => [],
+
+
+    'allowed_headers' => ['*'],
+
+
+    'exposed_headers' => [],
+
+
+    'max_age' => 0,
+
+  
+    'supports_credentials' => true,
+
+];
