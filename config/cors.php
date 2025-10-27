5e39725cceec0731887222b433e560d896dabf7f
abduelrhmnAllam <83561371+abduelrhmnAllam@users.noreply.github.com>
2025-10-27 05:04:31 +0300

    Major update:ssa

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
     'allowed_methods' => ['*'],
 
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://shopess.store'],
 
 
     'allowed_origins_patterns' => [],
@@ -31,7 +31,7 @@ return [
 
     'max_age' => 0,
 
-  
+
     'supports_credentials' => true,
 
 ];
